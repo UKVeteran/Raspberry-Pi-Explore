12414647c453847bbf1b29009e007522b449fc79
Johar M. Ashfaque <39216339+UKVeteran@users.noreply.github.com>
2021-10-18 18:09:50 +0100

    Add files via upload

---
+++ raspturtlexplore.py
@@ -0,0 +1,33 @@
+from turtle import *
+
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+
+from turtle import *
+colors = ['red', 'purple', 'blue', 'green', 'yellow', 'orange']
+for x in range(360):
+    pencolor(colors[x % 6])
+    width(x / 100 + 1)
+    forward(x)
+    left(59)
+    
+
+from turtle import *
+
+for i in range(10):
+    forward(100)
+    left(90)
+    forward(10)
+    left(90)
+    forward(100)
+    right(90)
+    forward(10)
+    right(90)
+
+for i in range(30):
+    undo()
